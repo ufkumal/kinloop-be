bccaaf9b502cd0ab7ee254e31f1f8a8866eb1242
UfkumDeniz <ufkum.deniz@gmail.com>
2026-07-06 21:57:03 +0300

    first api's register and login services are built. spring security configured.

--- src/main/java/com/kinloop/backend/exception/EmailAlreadyExistsException.java
+++ src/main/java/com/kinloop/backend/exception/EmailAlreadyExistsException.java
@@ -1,8 +1,7 @@
 package com.kinloop.backend.exception;
 
 public class EmailAlreadyExistsException extends RuntimeException {
-//TODO it is a vulnerability we expose the sensitive data information with this message. give each error message securely.
     public EmailAlreadyExistsException() {
-        super("Email already exists");
+        super("You can not do this transaction");
     }
 }
